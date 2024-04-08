52baa13839279f6aa5938c6bd5a81173472d5eac
evandrosalmeida <163478021+evandrosalmeida@users.noreply.github.com>
2024-04-07 21:03:49 -0300

    Delete GUI directory

--- GUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ControleStoke.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
